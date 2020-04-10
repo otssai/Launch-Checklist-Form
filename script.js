a6e53a495954a8d2f96226a2a23dec714921563d
Xiang Yu <yuxiang@Xiangs-MacBook-Air.local>
2020-04-09 20:38:37 -0500

    finished without bonus

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let launchStatus = document.getElementById("launchStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let faultyItems = document.getElementById('faultyItems');
+      
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();      
+      } else if (!isNaN(Number(pilotNameInput.value)) || !isNaN(Number(copilotNameInput.value)) || isNaN(Number(fuelLevelInput.value)) || isNaN(Number(cargoMassInput.value))) {
+         alert("Invalid info.");
+         event.preventDefault();
+      } else {
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotNameInput.value} is ready for launch`;
+         event.preventDefault();
+
+         if (fuelLevelInput.value<10000) {
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "red";
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+         } 
+         
+         if (cargoMassInput.value>10000) {
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "red";
+            cargoStatus.innerHTML = "Cargo mass too much for launch";
+         }
+
+         if (fuelLevelInput.value>=10000 && cargoMassInput.value<=10000) {
+            launchStatus.innerHTML = "Shuttle is ready for launch";
+            launchStatus.style.color = "green";
+         }
+
+      }
+
+      // if (typeof Number(pilotNameInput.value) === "number" || typeof Number(copilotNameInput.value) === "number" || typeof Number(fuelLevelInput.value) !== "number" || typeof Number(cargoMassInput.value) !== "number") {
+      //    alert("Invalid info.");    
+      //    event.preventDefault();         
+      // }
+
+   });
+});
+
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         const div = document.getElementById("missionTarget");
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[5].name}</li>
+               <li>Diameter: ${json[5].diameter}</li>
+               <li>Star: ${json[5].star}</li>
+               <li>Distance from Earth: ${json[5].distance}</li>
+               <li>Number of Moons: ${json[5].moons}</li>
+            </ol>
+            <img src="${json[5].image}">
+         `
+      })
+   })
+})
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
